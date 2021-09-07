afb5dae046581e73a4af502afdecc0a25f60ee2e
Ahmed Jazzar <11036472+ahmedaljazzar@users.noreply.github.com>
2021-09-07 14:13:29 -0700

    Init commit

---
+++ tahoe_auth0/__init__.py
@@ -0,0 +1,7 @@
+"""
+tahoe_auth0 initialization module
+"""
+
+# Increase this version by 1 after every backward-incompatible
+# change in the exported data format
+__version__ = '0.1.0-dev3'
---
+++ tahoe_auth0/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TahoeAuth0Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tahoe_auth0'
