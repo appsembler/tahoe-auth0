43f11a78acae551e4d764664261031aa0f03793b
Omar Al-Ithawi <i@omardo.com>
2022-03-29 10:32:47 +0300

    remove unneeded `openedx` mocks
    now we're using `site-configuration-client`

--- openedx/core/djangoapps/site_configuration/helpers.py
+++
@@ -1,8 +0,0 @@
-"""A mock for openedx site_configuration.helpers"""
-
-
-configurations = {"ENABLE_TAHOE_AUTH0": True}
-
-
-def get_value(value, default=None):
-    return configurations.get(value, default)
