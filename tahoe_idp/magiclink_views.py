86c76d6607187cfe38af85f7a6240b75a1962a62
Shadi Naif <shadinaif@gmail.com>
2022-05-25 18:59:24 +0300

    Studio SSO with MagicLink

--- tahoe_idp/magiclink_views.py
+++ tahoe_idp/magiclink_views.py
@@ -1,12 +1,16 @@
 import logging
 
+from django.conf import settings as django_settings
 from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
-from tahoe_idp import magiclink_settings as settings
+from tahoe_idp import magiclink_settings
+from tahoe_idp.magiclink_helpers import create_magiclink
 from tahoe_idp.magiclink_utils import get_url_path
 from tahoe_idp.models import MagicLink
 
@@ -21,7 +25,7 @@ class LoginVerify(TemplateView):
         username = request.GET['username']
         user = authenticate(request, token=token, username=username)
         if not user:
-            redirect_url = get_url_path(settings.LOGIN_FAILED_REDIRECT)
+            redirect_url = get_url_path(magiclink_settings.LOGIN_FAILED_REDIRECT)
             return HttpResponseRedirect(redirect_url)
 
         login(request, user)
@@ -34,4 +38,19 @@ class LoginVerify(TemplateView):
     def login_complete_action(self) -> HttpResponseRedirect:
         token = self.request.GET.get('token')
         magiclink = MagicLink.objects.get(token=token)
-        return HttpResponseRedirect(magiclink.redirect_url or settings.LOGIN_REDIRECT_URL)
+        return HttpResponseRedirect(magiclink.redirect_url or django_settings.LOGIN_REDIRECT_URL)
+
+
+class StudioLoginAPIView(View):
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StudioLoginAPIView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        username = request.user.username
+        next_url = request.GET.get('next') or '/home'
+
+        magic_link = create_magiclink(username=username, request=request, redirect_url=next_url)
+
+        url = magic_link.generate_url(request)
+        return redirect(url, permanent=False)
---
+++ tahoe_idp/tests/test_magiclink_studio_login.py
@@ -0,0 +1,27 @@
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from tahoe_idp import magiclink_settings as mlsettings
+from tahoe_idp.tests.magiclink_fixtures import user  # NOQA: F401
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+def test_studio_login_must_be_authenticated(client, settings):  # NOQA: F811
+    url = reverse('studio_login')
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.url.startswith(settings.LOGIN_URL)
+
+
+@pytest.mark.django_db
+def test_studio_login(client, user):  # NOQA: F811
+    url = reverse('studio_login')
+    client.login(username=user.username, password='password')
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.url.startswith('http://{studio_domain}'.format(studio_domain=mlsettings.STUDIO_DOMAIN))
