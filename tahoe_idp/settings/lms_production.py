c17aa7c364ba3c0a7e9e20f19948fb0f8a2e4bd5
Shadi Naif <shadinaif@gmail.com>
2022-06-23 18:45:33 +0300

    Fix THIRD_PARTY_AUTH_BACKENDS not found

--- tahoe_idp/settings/lms_production.py
+++ tahoe_idp/settings/lms_production.py
@@ -10,5 +10,5 @@ def plugin_settings(settings):
 
     # Add the Social / ThirdPartyAuth backend
     tahoe_idp_backend = 'tahoe_idp.backend.TahoeIdpOAuth2'
-    if tahoe_idp_backend not in settings.THIRD_PARTY_AUTH_BACKENDS:
-        settings.THIRD_PARTY_AUTH_BACKENDS.insert(0, tahoe_idp_backend)
+    if tahoe_idp_backend not in settings.AUTHENTICATION_BACKENDS:
+        settings.AUTHENTICATION_BACKENDS.insert(0, tahoe_idp_backend)
